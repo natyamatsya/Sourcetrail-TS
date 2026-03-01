999aafb56f5d3f9fbb373529b491e61698faaa10
natyamatsya <natyamatsya@users.noreply.github.com>
2026-03-01 23:35:29 +0100

    refactor: remove legacy ClangCompatibility shim files and CMake filter
    - Remove ClangCompatibility.h header file that redirected to ClangCompat.h
    - Remove ClangCompatibility.cpp compatibility shim file
    - Remove CMake filter that excluded ClangCompatibility.cpp from build

--- src/lib_cxx/data/parser/cxx/clang_compat/ClangCompatibility.cpp
+++
@@ -1,6 +0,0 @@
-#include "ClangCompatibility.h"
-
-// Compatibility shim only.
-// Real implementations live in:
-// - clang_compat/v22/ClangCompat.cpp
-// - clang_compat/v21/ClangCompat.cpp
--- src/lib_cxx/data/parser/cxx/clang_compat/ClangCompatibility.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLANG_COMPATIBILITY_H
-#define CLANG_COMPATIBILITY_H
-
-#include "ClangCompat.h"
-
-#endif	 // CLANG_COMPATIBILITY_H
